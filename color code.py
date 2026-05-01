7f11aafd518c0f707c8d56f320abba8cbf7cfb86
pravallikanagireddy7144-netizen <pravallikanagireddy7144@gmail.com>
2026-05-01 18:36:00 +0530

    Add files via upload

---
+++ color code.py
@@ -0,0 +1,17 @@
+c = input().strip().upper()
+if c == 'V':
+    print("Violet")
+elif c == 'I':
+    print("Indigo")
+elif c == 'B':
+    print("Blue")
+elif c == 'G':
+    print("Green")
+elif c == 'Y':
+    print("Yellow")
+elif c == 'O':
+    print("Orange")
+elif c == 'R':
+    print("Red")
+else:
+    print(-1)
